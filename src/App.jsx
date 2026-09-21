865c15c480b21c0d0b33d0e278e5875b20f47b4a
Do Trung Gia Bao <dtgb0905@gmail.com>
2026-09-21 09:01:26 +0700

    bài tập slot 5

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import WelcomeCard from './components/WelcomeCard';
+
+function App() {
+  return (
+    <>
+      <WelcomeCard />
+    </>
+  )
+}
+
+export default App
